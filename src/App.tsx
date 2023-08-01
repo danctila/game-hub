0837fd97103a1aa4ae6519492077168917097550
danctila <danctila@gmail.com>
2023-08-01 12:11:22 -0400

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`, //mobile formatting
         lg: `"nav nav" "aside main"`, //larger than 1024px
       }}
+      templateColums={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
